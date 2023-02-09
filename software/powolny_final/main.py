59784166bfe94db3f2d128468807132afc49678c
MrDusza <jozeksuperchlop@gmail.com>
2023-02-09 14:26:08 +0100

    added power handling

--- software/powolny_final/main.py
+++ software/powolny_final/main.py
@@ -2,6 +2,7 @@ import _thread
 from display_handling import display
 from temp_api import read_ds_sensor, change_temp_factor
 from output_api import output_state
+from power_api import power_calculation, current_calculation, voltage_calculation
 from time import sleep
 from machine import Timer, Pin, freq
 
@@ -13,6 +14,7 @@ global_time = 0
 set_temperature = 50
 water_temperature = 0.0
 program_status = 0
+current_value = 0.0
 
 #=========================================================================================================
 
@@ -26,16 +28,18 @@ global_timer.init(period=10, mode=Timer.PERIODIC, callback=global_time_inc)
 #==========================================================================================================
 
 def thread0():
+    global water_temperature
+    global set_temperature
+    global current_value
     while True:
-        global water_temperature
-        global set_temperature
         set_temperature = change_temp_factor(set_temperature)
         water_temperature = read_ds_sensor()
+        current_value = round(current_calculation(), 2)
 
 _thread.start_new_thread(thread0,())     
      
 while True:
     if global_time % 100 == 0:
-        display(water_temperature, set_temperature, program_status, global_time)
+        display(water_temperature, set_temperature, program_status, current_value)
     if global_time % 43 == 0:
         program_status = output_state(water_temperature, set_temperature)
---
+++ software/powolny_final/power_api.py
@@ -0,0 +1,47 @@
+from machine import Pin, ADC
+
+current = ADC(Pin(34))
+current.atten(ADC.ATTN_11DB)           
+current.width(ADC.WIDTH_10BIT)         
+
+voltage = ADC(Pin(33))
+voltage.atten(ADC.ATTN_11DB)           
+voltage.width(ADC.WIDTH_10BIT)         
+
+def calculate_current_adc_error(adc_value):
+    return (24.4
+            +  (0.0696 * adc_value)
+            +  (-0.000288 * adc_value * adc_value)
+            +  (0.000000415 * adc_value * adc_value * adc_value)
+            +  (-0.00000000026 * adc_value * adc_value * adc_value * adc_value))
+
+def calculate_voltage_adc_error(adc_value):
+    return (24.2
+            +  (0.0646 * adc_value)
+            +  (-0.000267 * adc_value * adc_value)
+            +  (0.000000385 * adc_value * adc_value * adc_value)
+            +  (-0.000000000246 * adc_value * adc_value * adc_value * adc_value))
+
+def current_calculation():
+    global current
+    
+    actual_current = current.read()
+    current_value = round(actual_current + calculate_current_adc_error(actual_current))
+    current_output = 617 - current_value + 155
+    
+    return current_output * 0.032415 
+
+def voltage_calculation():
+    global voltage
+    
+    actual_voltage = voltage.read()
+    voltage_value = round(actual_voltage + calculate_voltage_adc_error(actual_voltage))
+    
+    return voltage_value * 0.3473684
+    
+def power_calculation():
+    current_value = current_calculation()
+    voltage_value = voltage_calculation()
+    power_value = current_value * voltage_value
+    
+    return power_value
